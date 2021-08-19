369ec155c7f4bede381552704a6b7f8eb2a792bb
Carlos Eduardo Grell <carlosgrell@live.com>
2021-08-19 00:34:34 -0300

    Fixed plugins on hyper js

--- .hyper.js
+++ .hyper.js
@@ -115,7 +115,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-    plugins: ['hyper-search', 'hyperdocs'],
+    plugins: ['hyper-ayu', 'hyperdocs'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
